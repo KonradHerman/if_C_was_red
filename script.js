26a8e481a4c0de9c2bc648d1ad434aa51562c030
KonradHerman <k.radikal@gmail.com>
2023-09-10 18:43:34 -0400

    handle key like midi

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-import { noteToColorMap, keyboardToNoteMap } from './mappings.js';
+import { noteToColorMap, keyboardToNoteMap } from "./mappings.js";
 
 let firstNotePlayed = false;
 
-document.addEventListener("DOMContentLoaded", function() {
-
-  document.addEventListener('keydown', function(event) {
+document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("keydown", function (event) {
     const key = event.key.toLowerCase();
     if (keyboardToNoteMap.hasOwnProperty(key)) {
       const note = keyboardToNoteMap[key];
@@ -13,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
-  document.addEventListener('keyup', function(event) {
+  document.addEventListener("keyup", function (event) {
     const key = event.key.toLowerCase();
     if (keyboardToNoteMap.hasOwnProperty(key)) {
       const note = keyboardToNoteMap[key];
@@ -21,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
       handleMIDIMessage({ data: [0x80, note, 0] });
     }
   });
-
 });
 
 function handleMIDIMessage(event) {
@@ -30,7 +28,8 @@ function handleMIDIMessage(event) {
   let noteNumber = event.data[1];
   let velocity = event.data[2];
 
-  if (cmd === 9 && velocity > 0) {  // note on
+  if (cmd === 9 && velocity > 0) {
+    // note on
     if (!firstNotePlayed) {
       document.getElementById("heading").style.display = "none";
       document.getElementById("subheading").style.display = "none";
@@ -40,10 +39,11 @@ function handleMIDIMessage(event) {
     let noteBase = noteNumber % 12;
     if (noteToColorMap.hasOwnProperty(noteBase)) {
       let color = noteToColorMap[noteBase];
-      console.log(`Note on: ${noteNumber}, Color: ${color}`);  // Debug statement
+      console.log(`Note on: ${noteNumber}, Color: ${color}`); // Debug statement
       document.body.style.backgroundColor = color;
     }
-  } else if (cmd === 8 || (cmd === 9 && velocity === 0)) {  // note off
-    document.body.style.backgroundColor = '#ffffff';
+  } else if (cmd === 8 || (cmd === 9 && velocity === 0)) {
+    // note off
+    document.body.style.backgroundColor = "#ffffff";
   }
 }
