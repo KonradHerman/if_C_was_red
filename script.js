7666c98dd357961f4c143e9da28c3fb554ff9585
KonradHerman <k.radikal@gmail.com>
2023-09-10 19:09:34 -0400

    alpha

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
-import { noteToColorMap, keyboardToNoteMap } from "./mappings.js";
+import { noteToColorMap, keyboardToNoteMap } from './mappings.js';
 
 let firstNotePlayed = false;
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.addEventListener("keydown", function (event) {
-    const key = event.key.toLowerCase();
-    if (keyboardToNoteMap.hasOwnProperty(key)) {
-      const note = keyboardToNoteMap[key];
-      console.log(`Key down: ${key}, mapped to note: ${note}`); // Debug statement
-      handleMIDIMessage({ data: [0x90, note, 127] });
-    }
-  });
-
-  document.addEventListener("keyup", function (event) {
-    const key = event.key.toLowerCase();
-    if (keyboardToNoteMap.hasOwnProperty(key)) {
-      const note = keyboardToNoteMap[key];
-      console.log(`Key up: ${key}, mapped to note: ${note}`); // Debug statement
-      handleMIDIMessage({ data: [0x80, note, 0] });
-    }
-  });
-});
-
-function handleMIDIMessage(event) {
-  let cmd = event.data[0] >> 4;
-  let channel = event.data[0] & 0xf;
-  let noteNumber = event.data[1];
-  let velocity = event.data[2];
-
-  if (cmd === 9 && velocity > 0) {
-    // note on
-    if (!firstNotePlayed) {
-      document.getElementById("heading").style.display = "none";
-      document.getElementById("subheading").style.display = "none";
-      firstNotePlayed = true;
-    }
-
-    let noteBase = noteNumber % 12;
-    if (noteToColorMap.hasOwnProperty(noteBase)) {
-      let color = noteToColorMap[noteBase];
-      console.log(`Note on: ${noteNumber}, Color: ${color}`); // Debug statement
-      document.body.style.backgroundColor = color;
-    }
-  } else if (cmd === 8 || (cmd === 9 && velocity === 0)) {
-    // note off
-    document.body.style.backgroundColor = "#ffffff";
+function hideText() {
+  if (!firstNotePlayed) {
+    document.getElementById("heading").style.display = "none";
+    document.getElementById("subheading").style.display = "none";
+    firstNotePlayed = true;
   }
 }
+
+function handleMIDIMessage(message) {
+  const command = message.data[0];
+  const note = message.data[1];
+  const velocity = message.data[2];
+  
+  console.log(`MIDI Message received. Command: ${command}, Note: ${note}, Velocity: ${velocity}`);
+
+  if (command === 144) { // Note on
+    hideText();
+    changeBackgroundColor(note);
+  } else if (command === 128) { // Note off
+    // Do something for note off if you like
+  }
+}
+
+function changeBackgroundColor(note) {
+  const color = noteToColorMap[note % 12];
+  console.log(`Changing background color to ${color}`);
+  document.body.style.backgroundColor = color;
+}
+
+// Initialize MIDI
+navigator.requestMIDIAccess()
+  .then(onMIDISuccess, onMIDIFailure);
+
+function onMIDISuccess(midiAccess) {
+  for (const input of midiAccess.inputs.values()) {
+    input.onmidimessage = handleMIDIMessage;
+  }
+}
+
+function onMIDIFailure() {
+  console.log('Could not access your MIDI devices.');
+}
+
+// Handle keyboard input
+document.addEventListener('keydown', (event) => {
+  const note = keyboardToNoteMap[event.key];
+  
+  console.log(`Keyboard key pressed: ${event.key}, Mapped Note: ${note}`);
+
+  if (note !== undefined) {
+    hideText();
+    changeBackgroundColor(note);
+  }
+});
