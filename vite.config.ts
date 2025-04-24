a5591c3ee39e601e2ced860f8e2688c86ca4e9be
KonradHerman <konrad.herman@sequentum.com>
2025-04-24 16:27:33 -0400

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/if-c-was-red/',
+  base: '/if_c_was_red/',
 })
